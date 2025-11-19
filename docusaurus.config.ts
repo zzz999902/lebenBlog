81dd0eabe3a30aa09ddac8651fb095846b260a91
I certainly won't <81507123+zzz999902@users.noreply.github.com>
2025-11-19 12:45:39 +0800

    1

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -22,8 +22,8 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'zzz999902', // Usually your GitHub org/user name.
+  projectName: 'lebenBlog', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
